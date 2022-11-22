fd0b50a0bb146d84aff7419c35f620e25995d5fd
Akbar <akbar.119140004@student.itera.ac.id>
2022-11-22 08:51:21 +0700

    bug pembayaran

--- resources/views/holidays/index.blade.php
+++ resources/views/holidays/index.blade.php
@@ -9,7 +9,8 @@
     <div>
         <a href="{{ route('holidays.create') }}" class="btn btn-sm btn-primary">
             <span data-feather="plus-circle" class="align-text-bottom me-1"></span>
-            Tambah Data
+            Tambah Data 
+        </a>
     </div>
 </div>
 @endsection
